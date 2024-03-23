3b7ea6de5edb8b9499608ec9a979b19cd2764db5
Raphael Scandura <raphaelscandura@hotmail.com>
2024-03-23 06:34:59 -0300

    spring initlzr

---
+++ backend/src/main/java/com/scandura/tgid/TgidApplication.java
@@ -0,0 +1,13 @@
+package com.scandura.tgid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgidApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/scandura/tgid/TgidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scandura.tgid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
